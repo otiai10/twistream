b4a3522213e5a49fea0185a26324eae67960b3b1
otiai10 <otiai10@gmail.com>
2014-07-07 19:32:15 +0900

    Add test skel

---
+++ all_test.go
@@ -0,0 +1,17 @@
+package twistream_test
+
+import "testing"
+import . "github.com/otiai10/mint"
+import "github.com/otiai10/twistream"
+
+func TestNew(t *testing.T) {
+	timeline, e := twistream.New(
+		"https://userstream.twitter.com/1.1/user.json",
+		"consumer_key",
+		"consumer_secret",
+		"access_token",
+		"access_token_secret",
+	)
+	Expect(t, e).ToBe(nil)
+	Expect(t, timeline).TypeOf("*twistream.Timeline")
+}
