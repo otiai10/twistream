dd64ccd5c65a74f7bb5c24b023bf2887f028e971
otiai10 <otiai10@gmail.com>
2014-07-08 02:22:37 +0900

    Add example

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package twistream_test
+
+import "github.com/otiai10/twistream"
+import "testing"
+import "fmt"
+
+func ExampleTimeline_Listen(t *testing.T) {
+	timeline, _ := twistream.New(
+		"https://userstream.twitter.com/1.1/user.json",
+		"CONSUMER_KEY",
+		"CONSUMER_SECRET",
+		"ACCESS_TOKEN",
+		"ACCESS_TOKEN_SECRET",
+	)
+
+	status := <-timeline.Listen()
+	fmt.Println(status)
+}
